0da462f0b7f18be37d719e42de683378bca6d49b
FurTorie <75777516+FurTorie@users.noreply.github.com>
2026-09-26 09:36:44 +0000

    chore: update permissions and types

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -501,6 +501,7 @@ module.exports = {
       '*://matthewhotelscience.com/e/*',
       '*://jessicachoosemake.com/e/*',
       '*://stevenfamilyedge.com/e/*',
+      '*://jamesbornmain.com/e/*',
       // auto-voe-replace-dont-remove
     ],
   },
